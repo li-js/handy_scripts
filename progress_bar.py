61f6ccaed348fe3fbaca6d9580ae5ca59dc1df3d
Li Jianshu <lijianshu2015@gmail.com>
2017-05-14 20:28:36 +0800

    Update progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -8,7 +8,7 @@ from progressbar import ProgressBar, Bar, BouncingBar, Counter, ETA, Percentage,
 
 
 counter_total=500
-widgets=['Current: ', Counter(), '/{} |'.format(counter_total), Percentage(), ' |', Timer(), ' |', ETA(), ' |', Bar()]
+widgets=['Loading ..', Bar(), Counter(), '/{} |'.format(counter_total), Percentage(), ' |', Timer(), ' |', ETA()]
 pbar = ProgressBar(widgets=widgets, maxval=counter_total)
 for x in pbar(range(counter_total)): time.sleep(0.02)
 
