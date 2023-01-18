8ba76371347fd2cfecc3eaa09fc9dd0663228dcb
Li Jianshu <lijianshu2015@gmail.com>
2023-01-18 10:42:13 +0800

    Create cpp_debug.cpp

---
+++ cpp_debug.cpp
@@ -0,0 +1,25 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <math.h>
+
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  
+  std::string dir_img = "fdf";
+  
+  std::cout<<"LJS DDDDDD " << __FILE__<<":"<<__LINE__<<"   dir_img:"<<dir_img<<std::endl;    
+	
+
+  const float data[] = {
+		-0.016393604f, 0.016393604f, 0.0f, 0.0f,
+		0.0f, 0.0f, -0.0067330846f, 0.019016720f,
+		0.0f, 0.0f, -0.044531129f, 0.021565545f
+	};
+
+    std::cout<<"LJS DDDDDD " << __FILE__<<":"<<__LINE__<<"   data[4]:"<<data[4]<<std::endl;    
+
+ 
+}
